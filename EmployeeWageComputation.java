7310ce02093768a23c6511ba05cc3d00903b8688
vivektangudu2024 <tanguduvivek2003.com>
2023-11-23 04:58:11 +0530

    Added Welcome Message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.day3;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
