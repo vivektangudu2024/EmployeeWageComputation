51fd8b83e3b3eb3ea8c805b6ed4b331205b3f213
vivektangudu2024 <tanguduvivek2003.com>
2023-11-23 06:12:50 +0530

    Added UC3 Part time employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,36 +4,65 @@ import java.util.*;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
         // Constants
         int wagePerHour = 20;
         int fullDayHours = 8;
-        
-        // Calling the checkAttendance method to determine if the employee is present or absent
-        if (checkAttendance()) {
+        int partTimeHours = 4;
+
+        // Calling the checkAttendance method to determine the type of work (full-time, part-time, or absent)
+        int workingHours = checkAttendance(fullDayHours, partTimeHours);
+
+        if (workingHours > 0) {
             System.out.println("Employee is Present");
-            int dailyWage = calculateDailyWage(wagePerHour, fullDayHours);
-            System.out.println("Daily Employee Wage: " + dailyWage);
+
+
+
+            // Displaying the type of work
+            if (workingHours == fullDayHours) {
+                System.out.println("Type of Work: Full Time");
+            } else {
+                System.out.println("Type of Work: Part Time");
+            }
         } else {
             System.out.println("Employee is Absent");
+        }
+
+        // Calculate daily wage based on the type of work
+        int dailyWage = calculateDailyWage(wagePerHour, workingHours);
+        System.out.println("Daily Employee Wage: " + dailyWage);
+    }
+
+    /*
+     * @Desc : check employee attendance and return the number of working hours
+     * @Params: fullTimeHours, partTimeHours
+     * @returns: 0 for Absent, fullTimeHours for Full Time, partTimeHours for Part Time
+     */
+    public static int checkAttendance(int fullTimeHours, int partTimeHours) {
+        // Creating an instance of Random class
+        Random random = new Random();
+
+        // Generating a random number (0, 1, or 2) representing attendance
+        int attendance = random.nextInt(3);
+
+        // Returning the number of working hours based on the attendance value
+        switch (attendance) {
+            case 0:
+                return 0; // Absent
+            case 1:
+                return fullTimeHours; // Full Time
+            case 2:
+                return partTimeHours; // Part Time
+            default:
+                return 0; // Absent (default case)
         }
     }
 
     /*
-    * @Desc : check employee attendance using a random number
-    * @Params: 0
-    * @returns: Present/Absent
-    */
-    public static boolean checkAttendance() {
-        // Creating an instance of Random class
-        Random random = new Random();
-
-        // Generating a random number (0 or 1) representing attendance (0 for absent, 1 for present)
-        int attendance = random.nextInt(2);
-
-        // Returning true if attendance is 1 (present), false otherwise
-        return attendance == 1;
-    }
-
+     * @Desc : calculate daily wage
+     * @Params: wagePerHour, hoursWorked
+     * @returns: daily wage
+     */
     private static int calculateDailyWage(int wagePerHour, int hoursWorked) {
         return wagePerHour * hoursWorked;
     }
